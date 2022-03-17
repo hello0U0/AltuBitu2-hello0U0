096adca8f875a9ae1b40d0fecd9ed7a110e6ded1
hello0U0 <kleu358@gmail.com>
2022-03-17 13:29:04 +0900

    03월 11일 피드백 반영

--- 03월 11일 - 스택, 큐, 덱/1918.cpp
+++ 03월 11일 - 스택, 큐, 덱/1918.cpp
@@ -4,10 +4,20 @@
 
 using namespace std;
 
-char topPop(stack<char> &cal) {		//스택에서 값을 뺀다.
-	char ans=cal.top();
+char topPop(stack<char>& cal) {		//스택에서 값을 뺀다.
+	char ans = cal.top();
 	cal.pop();
 	return ans;
+}
+int priority(const char& cal) {			//우선순위를 정한다.
+	switch (cal) {
+	case '*': case '/':
+		return 1;
+	case '+':case '-':
+		return 2;
+	case '(': case ')':
+		return 3;
+	}
 }
 
 int main() {
@@ -17,16 +27,9 @@ int main() {
 	cin >> s;
 	for (int i = 0; i < s.size(); i++) {
 		char now = s[i];
-
 		switch (now) {
-		case '+': case '-':
-			while (!cal.empty() && cal.top() != '(') {
-				answer += topPop(cal);
-			}
-			cal.push(now);
-			break;
-		case '*': case '/':
-			while (!cal.empty()&&(cal.top()=='*' || cal.top() == '/')){
+		case '+': case '-': case '*': case '/':
+			while (!cal.empty() && priority(cal.top()) <= priority(now)) {
 				answer += topPop(cal);
 			}
 			cal.push(now);
