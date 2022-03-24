5164fdadc45e287598d1238c52e8d62dc6743ba3
hello0U0 <kleu358@gmail.com>
2022-03-24 18:44:34 +0900

    Update 10757.cpp

--- 03월 18일 - 브루트포스/10757.cpp
+++ 03월 18일 - 브루트포스/10757.cpp
@@ -9,16 +9,10 @@ int main() {
 	cin >> s1 >> s2;
 	//끝자리부터 덧셈계산을 한다.
 	while (!s1.empty() || !s2.empty()) {
-		char now = (!s1.empty() ? (s1.back() - '0') : 0) + (!s2.empty() ? (s2.back() - '0') : 0) + next;
-		if (now >= 10) {
-			next = 1;
-			now -= 10;
-		}
-		else {
-			next = 0;
-		}
-		now += '0';
-		ans = now + ans;
+		int now = (!s1.empty() ? (s1.back() - '0') : 0) + (!s2.empty() ? (s2.back() - '0') : 0) + next;
+		next = now / 10;
+		now %= 10;
+		ans = to_string(now) + ans;
 		if (!s1.empty()) s1.pop_back();
 		if (!s2.empty()) s2.pop_back();
 	}
