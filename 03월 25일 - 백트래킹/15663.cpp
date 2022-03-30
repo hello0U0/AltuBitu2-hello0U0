4e3b8f0ea032695a664f8eb7d8aa35a113ca7ced
hello0U0 <kleu358@gmail.com>
2022-03-31 07:36:20 +0900

    Create 15663.cpp

---
+++ 03월 25일 - 백트래킹/15663.cpp
@@ -0,0 +1,29 @@
+﻿//15663 N과 M (9)
+#include<iostream>
+#include<vector>
+#include<unordered_map>
+
+#include<algorithm>
+#include<string>
+using namespace std;
+
+int main() {
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	int n, m, x;
+	cin >> n >> m;
+	vector<int> num;
+	for (int i = 0; i < n; i++) {
+		cin >> x;
+		num.push_back(x);
+	}
+	sort(num.begin(), num.end());
+	do {
+		for (int i = 0; i < m; i++) {
+			cout << num[i]<<" ";
+		}
+		cout << "\n";
+		sort(num.begin() + m, num.end(),greater<>());
+	} while (next_permutation(num.begin(), num.end()));
+}
