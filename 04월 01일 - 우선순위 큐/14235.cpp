7c400d13be00271a01d5cabd0c93d080ce83b687
hello0U0 <kleu358@gmail.com>
2022-04-07 11:11:13 +0900

    0401_우선순위큐

---
+++ 04월 01일 - 우선순위 큐/14235.cpp
@@ -0,0 +1,34 @@
+﻿//14235 크리스마스선물
+#include<iostream>
+#include<queue>
+using namespace std;
+
+int main() {
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	int n, a;
+	priority_queue<int> present;
+	cin >> n;
+	while (n--) {
+		cin >> a;
+		if (a == 0) {
+			if (present.empty()) {
+				cout << "-1\n";
+			}
+			else {
+				cout << present.top() << '\n';
+				present.pop();
+			}
+			continue;
+		}
+		while (a--) {
+			int x;
+			cin >> x;
+			present.push(x);
+		}
+
+	}
+	return 0;
+}
