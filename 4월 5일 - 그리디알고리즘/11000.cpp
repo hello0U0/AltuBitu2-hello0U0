c504d446a963e6d18b5754206c4a938f3d882baa
hello0U0 <kleu358@gmail.com>
2022-04-11 11:50:59 +0900

    0405 제출

---
+++ 4월 5일 - 그리디알고리즘/11000.cpp
@@ -0,0 +1,32 @@
+﻿//11000 강의실 배정
+#include<iostream>
+#include<queue>
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	priority_queue<pair<int, int>> times;
+	priority_queue<int> lectures;
+	//강의들을 늦게 끝나는 순으로 배치
+	for (int i = 0; i < n; i++) {
+		int s, t;
+		cin >> s >> t;
+		times.push({t,s});	//강의 끝 시간, 시작 시간
+	}
+	//첫 강의 넣기
+	lectures.push(times.top().second);
+	times.pop();
+	while (!times.empty()) {
+		//빈 강의실에 넣기
+		pair<int,int> now = times.top();
+		times.pop();
+		if (now.first <= lectures.top()) {
+			lectures.pop();
+		}
+		lectures.push(now.second);
+	}
+
+	cout << lectures.size();
+	return 0;
+}
---
+++ 4월 5일 - 그리디알고리즘/13305.cpp
@@ -0,0 +1,44 @@
+﻿//13305 주유소
+#include<iostream>
+#include<queue>
+using namespace std;
+int main() {
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	int n, x;
+	cin >> n;
+	long long money = 0;
+	int town = n - 1;	//차의 위치
+	priority_queue<pair<int, int>,vector<pair<int,int>>,greater<>> oil;
+	int *load = new int[n-1];
+	//도시거리
+	for (int i = 0; i < n - 1; i++) {
+		cin >> load[i];
+	}
+	//기름값
+	for (int i = 0; i < n; i++) {
+		cin >> x;
+		oil.push(pair<int, int>(x, i));	//기름값,도시
+	}
+	//기름값이 작은 것부터
+	while (!oil.empty()) {
+		pair<int, int> now = oil.top();
+		oil.pop();
+		//이미 계산된 도시면 넘어가기
+		if (now.second >= town) continue;
+		long long nowmoney = 0;
+		// 현재 기름값의 도시부터 계산이 되지 않은 도시까지의 도로길이 * 기름값
+		for (int i = now.second; i < town; i++) {
+			nowmoney += load[i];
+		}
+		nowmoney *= now.first;
+		//비용을 계산한다.
+		money += nowmoney;
+		town = now.second;
+		//시작지점까지 갔으면 멈추기
+		if (town == 0) break;
+	}
+	cout << money;
+	return 0;
+}
